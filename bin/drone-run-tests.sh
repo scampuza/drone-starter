af6153d9edd1e9c3b2419d893e982fc47a0fa37f
scampuza <santiago@gumgum.com>
2018-11-15 05:04:47 -0800

    Added Key

--- bin/drone-run-tests.sh
+++ bin/drone-run-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-docker-compose --file docker-compose.drone-tests.yml rm -f # remove old containers
-docker-compose --file docker-compose.drone-tests.yml up --build
+docker-compose --file docker-compose.yml rm -f # remove old containers
+docker-compose --file docker-compose.yml up --build -d
 
 echo "Inspecting exited containers:"
-docker-compose --file docker-compose.drone-tests.yml ps
-docker-compose --file docker-compose.drone-tests.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
+docker-compose --file docker-compose.yml ps
+docker-compose --file docker-compose.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
     if [ "$code" != "0" ]; then
        exit $code
     fi
